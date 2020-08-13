73c92907a8850c8f5c5cf2779265e4c114069941
Susmit01 <66900417+Susmit01@users.noreply.github.com>
2020-08-13 20:15:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1= Math.random()*6;
+randomNumber1=Math.floor(randomNumber1)+1;
+var randomDiceImage= "images/dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var randomDiceImage2= "images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+if(randomNumber1 > randomNumber2){
+	document.getElementById("heading").innerHTML="Player 1 wins";
+	}else if (randomNumber1 < randomNumber2){
+		document.querySelector("h1").innerHTML="Player 2 wins";
+		}else{
+			document.querySelector("h1").innerHTML("Draw!");
+			}
